30e38e85a3e4b30015f47ec524725858424f269e
dorinengabdoh <ngabdohdorine@gmail.com>
2023-09-08 12:58:02 -0400

    initial commit

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# Secret-Handshake
+This little javascript project is about secret message print with some hand that is shake with a buttom `shake`.
+## Fonctionality
+- you can type your number on and input;
+- press on `share` and your your number is converted into binary and then reconverted by multiplying each binary number by the power of two, then displaying the following message `1 = wink
+10 = double blink
+100 = close your eyes
+1000 = jump
+10000 = Reverse the order of the operations in the secret handshake`
+- Then display on the last box;
+## Prerequisites
+- Node.js and npm must be installed on your machine.
+
+## Installation
+- Clone this repository on your machine.
+- Access the project directory in your terminal. 
+- Install the dependencies by running the npm install command.
+
+## usage
+- can be used for mathematicians;
+
+## an Image
+![alt text](./assets/images/Screenshot%20from%202023-09-08%2012-40-34.png).
---
+++ assets/scripts/script.js
@@ -0,0 +1,42 @@
+'use strict'
+// selecting elements from the DOM
+const input = document.getElementById('word')
+const output = document.getElementById('display')
+const but = document.getElementById('check-btn')
+const secretMessages = [
+  { number: 1, message: 'wink' },
+  { number: 10, message: 'double blink' },
+  { number: 100, message: 'close your eyes' },
+  { number: 1000, message: 'jump' }
+]
+const convertToBinary = (number) => {
+  let binary = ''
+  while (number > 0) {
+    binary = (number % 2) + binary
+    number = parseInt(number / 2)
+  }
+  return binary
+}
+document.addEventListener('DOMContentLoaded', () => {
+  const display = () => {
+    const binaryNumber = convertToBinary(input.value)
+    const arrBin = binaryNumber.split('') // split the binary string into an array
+    const reverseArrBin = arrBin.reverse()
+    const message = []
+    for (let i = 0; i < Math.min(reverseArrBin.length, secretMessages.length); i++) {
+      if (reverseArrBin[i] === '1') {
+        for (let i = 0; i < reverseArrBin.length; i++) {
+          if (i < secretMessages.length && reverseArrBin[i] === '1') {
+            message.push(secretMessages[i].message)
+          }
+        }
+      }
+      if (binaryNumber.length >= 5 && binaryNumber[binaryNumber.length - 5] === '1') {
+        message.reverse()
+      }
+    }
+
+    output.innerHTML = message.join(',')
+  }
+  but.addEventListener('click', display)
+})
